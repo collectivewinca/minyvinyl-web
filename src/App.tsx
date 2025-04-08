c642e71e9f4698a728abead49eb848055bb0bbeb
collectivewin <hello@collectivewin.ca>
2025-04-08 22:29:47 +0530

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import { Home } from './components/Home';
 import { CandleConcert } from './components/CandleConcert';
 import { NotFound } from './components/NotFound';
 import { Visitor } from './components/Visitor';
+import { ScrollToTop } from './components/ScrollToTop';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
